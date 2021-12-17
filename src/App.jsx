63d74adb6ef3b1dfc08439b46b90754379d2130b
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-12-16 23:32:26 -0800

    created item component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Shop from './views/Shop';
+
+function App() {
+  return (
+    <div className='App'>
+      <main className='App-main'>
+        <Shop />
+      </main>
+    </div>
+  );
+}
+
+export default App;
